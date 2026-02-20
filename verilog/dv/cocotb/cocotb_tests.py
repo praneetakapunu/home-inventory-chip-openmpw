89e441f279d1812f918f28e0f4095af644ba3eb9
exe.dev user <exedev@praneet.exe.xyz>
2026-02-20 00:31:25 +0000

    dv: enable home_inventory_wb_smoke in cocotb suite

--- verilog/dv/cocotb/cocotb_tests.py
+++ verilog/dv/cocotb/cocotb_tests.py
@@ -4,4 +4,5 @@ from user_proj_tests.counter_wb.counter_wb import counter_wb
 from user_proj_tests.counter_la.counter_la import counter_la
 from user_proj_tests.counter_la_reset.counter_la_reset import counter_la_reset
 from user_proj_tests.counter_la_clk.counter_la_clk import counter_la_clk
+from user_proj_tests.home_inventory_wb_smoke.home_inventory_wb_smoke import home_inventory_wb_smoke
 from gpio_test.gpio_test import gpio_test
